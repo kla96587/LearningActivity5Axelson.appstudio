ee7d53cc0e4b975813b9e9bd2a2c7998af4482ec
kla96587 <kla96587@creighton.edu>
2021-03-03 15:21:29 -0600

    branch 3

--- forms/Calc/Calc.js
+++ forms/Calc/Calc.js
@@ -1,10 +1,10 @@
-function calculate(num1, num2) {
+function calculate(num1, num2){
     let answer = num1 + num2
     return answer
 }
 
-let userNumber1 = prompt('Insert your first number here')
-let userNumber2 = prompt('Insert your second number here')
+let userNumber1 = parseInt(prompt('Insert your first number here'))
+let userNumber2 = parseInt(prompt('Insert your second number here'))
 let userAnswer = calculate(userNumber1, userNumber2)
 
-alert(`The sum of ${userNumber1} and ${userNumber2} is ${userAnswer}`)
+console.log(`The sum of ${userNumber1} and ${userNumber2} is ${userAnswer}`)
--- forms/While/While.js
+++ forms/While/While.js
@@ -3,5 +3,5 @@ let message = prompt('Please input number of iterations:')
 
 while (i < message) {
     i += 1
-    alert(`This loop iteration is ${i}`)
+    console.log(`This loop iteration is ${i}`)
 }

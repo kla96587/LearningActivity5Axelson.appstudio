138cf10e2b4937fb8132ead6521afa413f9796d7
kla96587 <kla96587@creighton.edu>
2021-03-03 15:17:04 -0600

    strings

---
+++ forms/Strings/Strings.js
@@ -0,0 +1,13 @@
+let quoteString = ('Failure is simply the opportunity to begin again this time more intelligently.')
+let authorString = ('- Henry Ford')
+let completeString = quoteString.concat(authorString)
+let found = completeString.includes('Henry')
+
+console.log(`The upper case string is ${quoteString.toUpperCase()}`)
+console.log(`Concatenated string is ${completeString}`)
+
+if (found == true) {
+console.log('Henry was in completeString.') 
+} else { 
+console.log('Henry was NOT in completeString.') 
+}
